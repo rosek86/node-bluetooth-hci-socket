2b577f2d37d3a303c327f7bc094e016e3e772225
Krzysztof Rosinski <rosek86@gmail.com>
2022-01-09 17:38:14 +0100

    fix hci socket factory

--- index.d.ts
+++ index.d.ts
@@ -4,42 +4,16 @@
 /// <reference types="node" />
 import { EventEmitter } from 'events';
 
-interface Device {
-    devId: number | null;
-    devUp: boolean | null;
-    /** USB-IF vendor ID. */
-    idVendor: number | null;
-    /** USB-IF product ID. */
-    idProduct: number | null;
-    /** Integer USB device number */
-    busNumber: number | null;
-    /** Integer USB device address */
-    deviceAddress: number | null;
-}
-
-interface BindParams {
-    usb: {
-        vid: number;
-        pid: number;
-        bus?: number;
-        address?: number;
-    }
-}
-
-declare namespace BluetoothHciSocket {
-    function bluetoothHciSocketFactory(type: 'native' | 'usb'): typeof BluetoothHciSocket;
-}
-
 declare class BluetoothHciSocket extends EventEmitter {
-    getDeviceList(): Device[];
+    getDeviceList(): BluetoothHciSocket.Device[];
     isDevUp(): boolean;
 
     start(): void;
     stop(): void;
     reset(): void;
 
-    bindRaw(devId: number, params?: BindParams): number;
-    bindUser(devId: number, params?: BindParams): number;
+    bindRaw(devId: number, params?: BluetoothHciSocket.BindParams): number;
+    bindUser(devId: number, params?: BluetoothHciSocket.BindParams): number;
     bindControl(): number;
 
     setFilter(filter: Buffer): void;
@@ -49,4 +23,50 @@ declare class BluetoothHciSocket extends EventEmitter {
     on(event: "error", cb: (error: NodeJS.ErrnoException) => void): this;
 }
 
+declare namespace BluetoothHciSocket {
+    export interface Device {
+        devId: number | null;
+        devUp: boolean | null;
+        /** USB-IF vendor ID. */
+        idVendor: number | null;
+        /** USB-IF product ID. */
+        idProduct: number | null;
+        /** Integer USB device number */
+        busNumber: number | null;
+        /** Integer USB device address */
+        deviceAddress: number | null;
+    }
+
+    export interface BindParams {
+        usb: {
+            vid: number;
+            pid: number;
+            bus?: number;
+            address?: number;
+        }
+    }
+
+    export function bluetoothHciSocketFactory(type: 'native' | 'usb'): BluetoothHciSocket;
+
+    export class BluetoothHciSocket extends EventEmitter {
+        getDeviceList(): BluetoothHciSocket.Device[];
+        isDevUp(): boolean;
+
+        start(): void;
+        stop(): void;
+        reset(): void;
+
+        bindRaw(devId: number, params?: BluetoothHciSocket.BindParams): number;
+        bindUser(devId: number, params?: BluetoothHciSocket.BindParams): number;
+        bindControl(): number;
+
+        setFilter(filter: Buffer): void;
+        write(data: Buffer): void;
+
+        on(event: "data", cb: (data: Buffer) => void): this;
+        on(event: "error", cb: (error: NodeJS.ErrnoException) => void): this;
+    }
+}
+
+export as namespace BluetoothHciSocket;
 export = BluetoothHciSocket;
--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const os = require('os');
 const platform = os.platform();
 
 if (process.env.BLUETOOTH_HCI_SOCKET_FACTORY) {
-  exports.bluetoothHciSocketFactory = function (type) {
-    if (type === 'native') {
-      if (platform === 'linux' || platform === 'android') {
-        return require('./lib/native');
+  module.exports = {
+    bluetoothHciSocketFactory: function (type) {
+      if (type === 'native') {
+        if (platform !== 'linux' && platform !== 'android') {
+          throw new Error('Unsupported platform');
+        }
+        const BluetoothHciSocket = require('./lib/native.js');
+        return new BluetoothHciSocket();
       } else {
-        return require('./lib/unsupported');
+        const BluetoothHciSocket = require('./lib/usb.js');
+        return new BluetoothHciSocket();
       }
-    } else {
-      return require('./lib/usb.js');
-    }
+    },
   };
 } else {
   if (process.env.BLUETOOTH_HCI_SOCKET_FORCE_USB || platform === 'win32' || platform === 'freebsd') {
